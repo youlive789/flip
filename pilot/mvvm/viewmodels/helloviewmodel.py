b74a7dacb2576276a6506fcc005461a1c356410d
Geon-Woo Kim <youlive789@gmail.com>
2020-08-11 13:06:58 +0900

    feat: Study kivy event dispatcher

--- pilot/mvvm/viewmodels/helloviewmodel.py
+++ pilot/mvvm/viewmodels/helloviewmodel.py
@@ -1,4 +1,9 @@
+from kivy.properties import BoundedNumericProperty
 from kivy.event import EventDispatcher
 
 class HelloViewModel(EventDispatcher):
-    pass
+    click_property = BoundedNumericProperty(1, min=0, max=5)
+    
+    def update_click_property(self, btn):
+        print("들어옴", btn)
+        self.click_property += 1
--- pilot/mvvm/views/helloview.py
+++ pilot/mvvm/views/helloview.py
@@ -1,6 +1,7 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.uix.recycleview import RecycleView
+from kivy.app import App
+from kivy.clock import Clock
 
 Builder.load_file("views/kv/hello.kv")
 
@@ -8,6 +9,16 @@ class HelloView(Screen):
 
     def __init__(self, **kwargs):
         super(HelloView, self).__init__(**kwargs)
+        Clock.schedule_once(lambda dt : self.prepare())
 
+    def prepare(self):
+        app = App.get_running_app()
+        app.hello_view_model.bind(
+            click_property=lambda o,v: print(o,v)
+        )
+
+    def button_clicked(self, btn):
+        app = App.get_running_app()
+        app.hello_view_model.update_click_property(btn)
     
 
