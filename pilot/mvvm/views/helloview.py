720dac5e5b06a1e1b06f3cbb1ecb547b58cbc5a5
Geon-Woo Kim <youlive789@gmail.com>
2020-08-14 11:20:53 +0900

    feat: Fix continuous keyborad down event

--- pilot/mvvm/views/helloview.py
+++ pilot/mvvm/views/helloview.py
@@ -10,6 +10,7 @@ class HelloView(Screen):
 
     def __init__(self, **kwargs):
         super(HelloView, self).__init__(**kwargs)
+        self.is_pressing = False
         Clock.schedule_once(lambda dt : self.prepare())
 
     def prepare(self):
@@ -21,5 +22,8 @@ class HelloView(Screen):
 
     def keyboard_test(self, e):
         app = App.get_running_app()
-        if e.name == 'ctrl' and e.event_type == "down":
+        if e.name == 'ctrl' and e.event_type == "down" and not self.is_pressing:
+            self.is_pressing = True
             print(app.hello_view_model.hello.get_recognized_text())
+        elif e.name == 'ctrl' and e.event_type == "up":
+            self.is_pressing = False
