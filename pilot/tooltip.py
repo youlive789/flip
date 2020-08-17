7bf7bbfc698d71fb2bae0b280a14169416fac271
gwkim <youlive789@gmail.com>
2020-08-17 17:18:03 +0900

    feat: Have to find how to implement windows tooltip using win32gui

---
+++ pilot/tooltip.py
@@ -0,0 +1,29 @@
+import win32con, win32gui
+from win32api import GetModuleHandle
+from commctrl import (TOOLTIPS_CLASS, TTS_ALWAYSTIP, TTS_NOPREFIX, TTM_ADDTOOL, TTM_SETMAXTIPWIDTH)
+
+class TooltipWindow:
+    def __init__(self):
+        win32gui.InitCommonControls() # Loads COMCTL32.DLL (Shell Common Controls Library)
+        self.hwnd = win32gui.CreateWindowEx(
+            win32con.WS_EX_CLIENTEDGE | win32con.WS_EX_TOPMOST,
+            TOOLTIPS_CLASS,
+            "MyTooltipWindow",
+            win32con.WS_POPUP | TTS_ALWAYSTIP | TTS_NOPREFIX | win32con.WS_BORDER,
+            win32con.CW_USEDEFAULT,
+            win32con.CW_USEDEFAULT,
+            win32con.CW_USEDEFAULT,
+            win32con.CW_USEDEFAULT,
+            None,
+            None,
+            GetModuleHandle(None),
+            None
+        )
+
+        win32gui.SetWindowPos(self.hwnd, win32con.WS_EX_TOPMOST, 0, 0, 0, 0, 
+                        win32.con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOACTIVATE)
+
+        win32gui.SendMessage(self.hwnd, TTM_ADDTOOL, None, 0)
+
+if __name__ == "__main__":
+    tooltip = TooltipWindow()
--- view/mainview.py
+++ view/mainview.py
@@ -1,13 +1,18 @@
 import keyboard
+import win32gui
 
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
 
 Builder.load_file("view/kv/main.kv")
 
 class MainView(Screen):
+
+    tooltip = None
 
     def __init__(self, **kwargs):
         super(MainView, self).__init__(**kwargs)
@@ -21,6 +26,13 @@ class MainView(Screen):
         app = App.get_running_app()
         if e.name == 'ctrl' and e.event_type == "down" and not self.is_pressing:
             self.is_pressing = True
-            print(app.main_view_model.ocr.get_recognized_text())
+            read_text = app.main_view_model.ocr.get_recognized_text()
+            print(read_text)
+
+            _, _, (x,y) = win32gui.GetCursorInfo()
+            self.tooltip = Popup(content=Label(text=read_text))
+            self.tooltip.open()
+
         elif e.name == 'ctrl' and e.event_type == "up":
             self.is_pressing = False
+            self.tooltip.dismiss()
